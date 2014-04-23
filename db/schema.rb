b50701c3d3cd3349b6c0e941ddc5041f7f1f65c6
Belov Vladislav <mrspy92@yandex.ru>
2014-04-23 15:29:24 +0400

    REALLY END OF THE 8'th CHAPTER

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319122600) do
+ActiveRecord::Schema.define(version: 20140423111424) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
     t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
