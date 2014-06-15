b677f6614a4f06f8880e21819c3f9592ebf366fd
Belov Vladislav <mrspy92@yandex.ru>
2014-06-15 08:41:42 +0400

    finish

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,14 @@ module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 50 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = user.admin
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+    if gravatar_id == true
+      then gravatar_url = "http://cs618518.vk.me/v618518451/10fe1/v7FkEWT65Uc.jpg "
+      # else gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      else gravatar_url = "http://cs618518.vk.me/v618518451/10fe8/R9aKyu7e7CY.jpg"
+    end
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
